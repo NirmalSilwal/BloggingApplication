dc67dff684518551bd044edda6df9485f07ac972
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-06-13 21:20:37 -0500

    project setup for blogging application

---
+++ src/test/java/com/eaproject/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaproject.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
